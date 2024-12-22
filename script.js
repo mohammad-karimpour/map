66bc32483d08c2de32b898b39f18b3c88b1f3843
MohammadKarimpour <89665384+mohammad-karimpour@users.noreply.github.com>
2024-12-22 16:02:44 +0330

    Update script.js

--- script.js
+++ script.js
@@ -385,7 +385,7 @@ let run_navigator_user = async (MQ_lat, MQ_lon) => {
 
             // به روزرسانی موقعیت نشانگر
             user_navigator_location.setLatLng([lat, lon]);
-            routeControl.route();
+            routeControl.setRouteWhileDragging(true);
 
             //routeControl.setWaypoints([L.latLng(lat, lon), routeControl.getWaypoints()[1]]);
             map.panTo([lat, lon]);
